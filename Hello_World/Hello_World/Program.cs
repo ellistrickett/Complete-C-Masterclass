3aeaf72cbcf85ab01843910b455faf7b6d3fcaff
Ellis Trickett <ellis.trickett@gmail.com>
2020-12-20 14:48:41 +0000

    void methods

--- Hello_World/Hello_World/Program.cs
+++ Hello_World/Hello_World/Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 
 namespace Hello_World
-{  // Constants are immutable values which are known
-    // at compile time and do not change for the life of the program
+{ 
     class Program
     {
-        // constants as fields
-        const double pI = 3.14159265359;
-        const int weeks = 52, month = 12;
-        const string birthday = "24.10.1995";
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("My Birthday is always going to be: {0}", birthday);
-            Console.ReadKey();
+            WriteSomething();
+            WriteSomethingSpecific("I am an argument and am called from a method");
+            Console.Read();
+        }
+        // access modifier (static) return type method name (parameter1, parameter2)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+        }
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
         }
     }
 }
