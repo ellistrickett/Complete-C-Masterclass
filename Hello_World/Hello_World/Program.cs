f63d58decb8227162e91814a16bd492508e28b3c
Ellis Trickett <ellis.trickett@gmail.com>
2020-12-20 13:15:24 +0000

    hellow world

---
+++ Hello_World/Hello_World/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num1 = 13;
+            int num2 = 5;
+            int sum = num1 + num2;
+            
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumD = d1 / d2;
+           
+            float f1 = 3.5F;
+            //Use casting
+            //int divDandI = d1 / num2;
+
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
+            Console.WriteLine(d1 + " divided by " + " and " + num2 + " is " + sumD);
+            Console.Read();
+        }
+    }
+}
