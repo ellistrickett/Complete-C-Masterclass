8e05dada52b0fce5fc415d30c87ff2191be924ef
Ellis Trickett <ellis.trickett@gmail.com>
2020-12-20 20:26:01 +0000

    our first own class

---
+++ Classes__Basics/Classes__Basics/Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Classes__Basics
+{
+    // this class is a blueprint for a datatype
+    class Human
+    {   
+        // member variable
+        public string firstName = "Michael";
+        public string lastName = "Jones";
+
+        // member
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I'm {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ Classes__Basics/Classes__Basics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Classes__Basics
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Create an object of my class
+            // an instance of Human
+            Human denis = new Human();
+            // access public variable from outside, and even change it
+            denis.firstName = "Denis";
+            denis.lastName = "Smith";
+            // call method of a class
+            denis.IntroduceMyself();
+
+            Human michael = new Human();
+            michael.IntroduceMyself();
+
+            Console.ReadKey();
+        }
+    }
+}
