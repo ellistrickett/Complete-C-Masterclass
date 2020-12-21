70497842077802ec27a33c90eb559e84d7eb1346
Ellis Trickett <ellis.trickett@gmail.com>
2020-12-21 14:16:40 +0000

    multidimensional arrays

--- _07___Arrays/_07___Arrays/Program.cs
+++ _07___Arrays/_07___Arrays/Program.cs
@@ -6,15 +6,36 @@ namespace _07___Arrays
     {
         static void Main(string[] args)
         {
-            string[] friends = { "Ellis", "Steve", "Michael", "Jack", "June" };
+            // Declare 2D array
+            string[,] matrix;
 
-            int counter = 0;
-            foreach(string i in friends)
+            //3D Array
+            int[,,] threeD;
+
+            // two dimensional array
+            int[,] array2D = new int[,]
             {
-                Console.WriteLine("Hey {0}", i);
-                counter++;
-            }
+                {1, 2, 3}, // row 0
+                {2, 5, 6}, // row 1
+                {7, 8, 9}  // row 2
+            };
 
+            string[,,] array3D = new string[,,]
+            {
+                {
+                    {"000", "001"},
+                    {"010", "011"}
+                },
+                {
+                    {"100", "101"},
+                    {"110", "111"}
+                }
+            };
+
+            // Rank returns the amount of dimensions
+            Console.WriteLine(array3D.Rank);
+
+            Console.WriteLine("Central value is {0}", array3D[1, 1, 0]);
             Console.ReadKey();
         }
     }
